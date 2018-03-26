5f0575bb086618e531fe646d5652ebe50fdcb284
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-26 23:55:03 +0300

    add test

--- tests.py
+++ tests.py
@@ -7,17 +7,35 @@ class TestFormatPrice(unittest.TestCase):
     def test_float_str(self):
         self.assertEqual(format_price('3245.000000'), '3 245')
 
+    def test_float_value(self):
+        self.assertEqual(format_price(3245.50000), '3 245')
+
+    def test_negative_float(self):
+        self.assertEqual(format_price(-3245.400000), '-3 245')
+
     def test_int_str(self):
         self.assertEqual(format_price('3245'), '3 245')
-
-    def test_float_value(self):
-        self.assertEqual(format_price(3245.000000), '3 245')
 
     def test_int_value(self):
         self.assertEqual(format_price(3245), '3 245')
 
+    def test_negative_int(self):
+        self.assertEqual(format_price(-3245), '-3 245')
+
     def test_string_text(self):
-        self.assertEqual(format_price('test'), None)
+        self.assertEqual(format_price('t55est'), None)
+
+    def test_string_int_long_number(self):
+        self.assertEqual(format_price('100_000_000'), '100 000 000')
+
+    def test_int_long_number(self):
+        self.assertEqual(format_price(100_000_000), '100 000 000')
+
+    def test_string_float_long_number(self):
+        self.assertEqual(format_price('100_000_000.4444'), '100 000 000')
+
+    def test_float_long_number(self):
+        self.assertEqual(format_price(100_000_000.60), '100 000 000')
 
 
 if __name__ == '__main__':
