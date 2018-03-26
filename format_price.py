007cd3bb454e418ce9d57e0c0d187d89c50e9637
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-26 16:07:29 +0300

    init commit

--- format_price.py
+++ format_price.py
@@ -1,5 +1,24 @@
+import sys
+import locale
+from unicodedata import normalize
+
+
 def format_price(price):
-    pass
+    try:
+        number_price = float(price)
+    except ValueError:
+        return None
+    locale.setlocale(locale.LC_ALL, 'ru_RU')
+    str_price = locale.format_string('%d', number_price, grouping=True)
+    return normalize("NFKD", str_price)
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        answer = format_price(sys.argv[1])
+        if not answer:
+            print("Can't be converted")
+        else:
+            print(answer)
+    except IndexError:
+        print('Not entered price')
--- tests.py
+++ tests.py
@@ -1,1 +1,24 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_float_str(self):
+        self.assertEqual(format_price('3245.000000'), '3 245')
+
+    def test_int_str(self):
+        self.assertEqual(format_price('3245'), '3 245')
+
+    def test_float_value(self):
+        self.assertEqual(format_price(3245.000000), '3 245')
+
+    def test_int_value(self):
+        self.assertEqual(format_price(3245), '3 245')
+
+    def test_string_text(self):
+        self.assertEqual(format_price('test'), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
