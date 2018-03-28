0e5ce93fa909febb8cf77b673557035ee82d4b60
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-28 08:27:58 +0300

    fix output

--- format_price.py
+++ format_price.py
@@ -1,18 +1,23 @@
 import sys
 
 
+def is_integer(num):
+    if num % 1 < 0.005:
+        return True
+
+
 def format_price(price):
     if isinstance(price, bool):
         return None
     try:
         price_as_number = float(price)
-        if price_as_number.is_integer():
-            str_price = '{0:,}'.format(int(price_as_number))
-        else:
-            str_price = '{0:,.2f}'.format(price_as_number)
-        return str_price.replace(',', ' ')
     except (ValueError, TypeError):
         return None
+    if is_integer(price_as_number):
+        str_price = '{0:,}'.format(int(price_as_number))
+    else:
+        str_price = '{0:,.2f}'.format(price_as_number)
+    return str_price.replace(',', ' ')
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -8,13 +8,13 @@ class TestFormatPrice(unittest.TestCase):
         self.assertEqual(format_price('3245.000000'), '3 245')
 
     def test_float_str_2(self):
-        self.assertEqual(format_price('3245.0000001'), '3 245.00')
+        self.assertEqual(format_price('3245.0000001'), '3 245')
 
     def test_float_str_3(self):
         self.assertEqual(format_price('3245.0000000000001'), '3 245')
 
     def test_float_str_4(self):
-        self.assertEqual(format_price('0.0000000000001'), '0.00')
+        self.assertEqual(format_price('0.0000000000001'), '0')
 
     def test_float_less_1_str(self):
         self.assertEqual(format_price('0.34234324'), '0.34')
@@ -26,9 +26,12 @@ class TestFormatPrice(unittest.TestCase):
         self.assertEqual(format_price(3245.00000000000005), '3 245')
 
     def test_float_value_3(self):
-        self.assertEqual(format_price(0.0000000000005), '0.00')
+        self.assertEqual(format_price(0.004), '0')
 
     def test_float_value_4(self):
+        self.assertEqual(format_price(0.005), '0.01')
+
+    def test_float_value_5(self):
         self.assertEqual(format_price(0.0000000000000), '0')
 
     def test_negative_float(self):
